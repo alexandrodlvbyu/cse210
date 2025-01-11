a8d10c2be308ce78dd84079570f3bf8d21d310b2
alexandrodlvbyu <alexandrodlv@byui.edu>
2025-01-11 18:20:24 +0100

    Exercise 1

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Sandbox Project.");
+        Console.WriteLine("This is un C#");
+
+     
+        
+        
+        Console.Write("What is your favorite color?");
+        string color = Console.ReadLine();
+        Console.WriteLine($"Your color is  {color}");
     }
 }
